5d0f11073d32145cb84a74e34bab53485dbce01f
“Silje <siljeeslettebakk@gmail.com>
2023-10-05 08:51:50 +0200

    Fix: mapping over test data for skills and profile information and changed the color for boxes and text

--- Lagalt_frontend/lagalt/src/components/profile/ProfileHeader.tsx
+++ Lagalt_frontend/lagalt/src/components/profile/ProfileHeader.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import ProfileHeaderItems from "./ProfileHeaderItems.tsx";
 
 const ProfileInfoWrapper = styled.div`
     background-color: #28113e;
@@ -7,16 +8,15 @@ const ProfileInfoWrapper = styled.div`
     height: 100px;
     padding-left: 50px;
     padding-top: 40px;
-    margin: 20px;
-    border-radius: 10px;
+    border-radius: 20px;
     list-style: none;
 
-    p {
+    li {
         text-decoration: none;
-        color: white;
+        color: #e7daf5;
         position: relative;
         left: 100px;
-        bottom: 62px;
+        bottom: 45px;
         font-size: 20px;
       }
 `;
@@ -27,25 +27,41 @@ const Image = styled.img`
     padding-left: ;
 `
 
-const test= [
-    {
-        name: "Silje D",
-        about: "Frontend utvikler med en bachelorgrad innen Frontend fra Høyskolen Kristiania",
-        skills: ["React", "JavaScript", "TypeScript", ".NET", "Angular"],
-    },
-    {
-        name: "Silje",
-        about: "Infoviter med en bachelorgrad innen Informasjonsvitenskap fra Univeristetet i Bergen",
-        skills: ["React", "JavaScript", "TypeScript", ".NET"],
-    },
-];
 
 const UserInfo = () => {
-    return(
-        <ProfileInfoWrapper>
-            <Image src="./../assets/default-profile-picture.png" alt="Default profile picture"/>
-            <p>Silje Slettebakk</p>
-        </ProfileInfoWrapper>
+
+    const test = [
+        {
+            name: "Silje D",
+            about: "Frontend utvikler med en bachelorgrad innen Frontend fra Høyskolen Kristiania",
+            skills: "React, JavaScript, TypeScript, .NET, Angular",
+        },
+        {
+            name: "Silje",
+            about: "Infoviter med en bachelorgrad innen Informasjonsvitenskap fra Univeristetet i Bergen",
+            skills: "React, JavaScript, TypeScript, .NET, Angular",
+        },
+    ];
+
+    return (
+        <ul>
+            <ProfileInfoWrapper>
+            <Image src="./../../assets/default-profile-picture.png" alt="Default profile picture"/>
+            {test.map((item, index) => {
+                if (item.name && index === 0) {
+                    return(
+                        <li key={index}>{item.name}</li>
+                    )
+                }
+            })}
+            </ProfileInfoWrapper>
+        </ul>
     );
-};
+}
+
+//             <Image src="./../../assets/default-profile-picture.png" alt="Default profile picture"/>
+//             <p>Silje Slettebakk</p>
+//         </ProfileInfoWrapper>
+//     );
+// };
 export default UserInfo;
--- Lagalt_frontend/lagalt/src/components/profile/ProfileSkills.tsx
+++ Lagalt_frontend/lagalt/src/components/profile/ProfileSkills.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import UserInfo from "./ProfileHeader.tsx";
 
 const SkillsWrapper = styled.div`
     background-color: #28113e;
@@ -7,36 +8,58 @@ const SkillsWrapper = styled.div`
     height: 300px;
     padding-left: 50px;
     padding-top: 10px;
-    margin: 20px;
-    border-radius: 10px;
+    border-radius: 20px;
     list-style: none;
+    margin-top: 20px;
 
     h2{
-        color: white;
+        color: #e7daf5;
     }
 
-    p {
-        color: white;
+    li{
+        line-height: 35px;
     }
 `;
 
-const SkillsHeader = styled.h2`
-    font-size: 15px;
-`
+const SkillsButton = styled.button`
+    background-color: #7834bb;
+    border: none;
+    color: #e7daf5;
+    font-size: 16px;
+    border-radius: 20px;
+    padding: 10px 15px;
+    margin-left: 190px;
+    height: fit-content;
+
+    &:hover{
+        background-color: #975dd2;
+    }
+`;
 
 const Skills = () => {
-    return(
-        <SkillsWrapper>
-            <li>
-                <SkillsHeader>
-                    <h2>Mine skills:</h2>
-                </SkillsHeader>
-                <p>JavaScript</p>
-                <p>C#</p>
-                <p>.NET</p>
-                <p>React</p>
-            </li>
-        </SkillsWrapper>
+
+    const tests = [
+        {skills: "React"}, 
+        {skills: "JavaScript"},
+        {skills: "TypeScript"},
+        {skills: ".NET"},
+        {skills: "Angular"}
+    ]
+
+    return (
+        <ul>
+            <SkillsWrapper>
+                <h2>Mine skills:</h2>
+                {tests.map((item, index) => {
+                    if (item.skills) {
+                        return(
+                            <li key={index}>{item.skills}</li>
+                        )
+                    }
+                })}
+                <SkillsButton>Add skill</SkillsButton>
+            </SkillsWrapper>
+        </ul>
     );
 };
 export default Skills;
