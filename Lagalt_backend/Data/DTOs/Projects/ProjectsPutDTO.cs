4356503c095ac81ba385f67e513edde5ade9718d
Magnus Uttisrud <magnus.uttisrud@no.experis.com>
2023-10-17 10:05:24 +0200

    Changing put project

--- Lagalt_backend/Data/DTOs/Projects/ProjectsPutDTO.cs
+++ Lagalt_backend/Data/DTOs/Projects/ProjectsPutDTO.cs
@@ -1,6 +1,5 @@
 ﻿namespace Lagalt_backend.Data.DTOs.Projects {
     public class ProjectsPutDTO {
-        public int Id { get; set; }
         public string? FullDescription { get; set; }
         public int? Progress { get; set; }
 
